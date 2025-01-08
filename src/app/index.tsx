59c1c0b9f351968913deecb63b415c6c922bdd51
arkajitCN <arkajit@capitalnumbers.com>
2025-01-08 18:34:18 +0530

    feat: add initial setup and configuration of the application

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <h3>React Flow</h3>
+      <Button>Click me</Button>
+    </div>
+  );
+}
